804d7aae43b775d645c33505252f2dd6b223e5e0
grtvd <grtvd@users.noreply.github.com>
2005-09-26 17:24:39 -0400

    mce: Updated SearchResultsScreen with format that considers SVP, removed Provider from list, add details to bottom of page.  Added sort buttons (not implemented). Added styles for small text in lists.

--- resources/search/SearchResultsScreen.js
+++ resources/search/SearchResultsScreen.js
@@ -5,13 +5,21 @@
 
 SearchResultsScreen.ScreenID = "Search003";
 SearchResultsScreen.ShowListID = "Search003_ShowList";
+SearchResultsScreen.NameID = "Search003_Name";
+SearchResultsScreen.EpisodeNameID = "Search003_EpisodeName";
+SearchResultsScreen.ProviderID = "Search003_Provider";
+SearchResultsScreen.SortByNameID = "Search003_SortByName";
+SearchResultsScreen.SortByPriceID = "Search003_SortByPrice";
 SearchResultsScreen.NoShowsTextID = "Search003_NoShowsText";
 
 /******************************************************************************/
 
 SearchResultsScreen.newInstance = function(/*Array*/ showSearchList)
 {
-	return MainApp.getThe().openScreen(new SearchResultsScreen(showSearchList));
+	var oScreen = new SearchResultsScreen(showSearchList);
+	MainApp.getThe().openScreen(oScreen);
+	oScreen.focusControl(SearchResultsScreen.ShowListID, true);
+	return oScreen;
 }
 
 /******************************************************************************/
@@ -26,20 +34,33 @@ function SearchResultsScreen(/*Array*/ showSearchList)
 	this.ScreenID = SearchResultsScreen.ScreenID;
 
 	var oRowItemList = new Array();
-	oRowItemList.push(new ListControlRowItem("Show", 522));
-	oRowItemList.push(new ListControlRowItem("Year", 95));
-	oRowItemList.push(new ListControlRowItem("Provider", 215));
-	oRowItemList.push(new ListControlRowItem("Cost", 120));
+	oRowItemList.push(new ListControlRowItem("Show", 500));
+	oRowItemList.push(new ListControlRowItem("Year", 80));
+	oRowItemList.push(new ListControlRowItem("Cost", 100));
 
 	this.fContainerControl = new ContainerControl(this.ScreenID, 30, 120);
+	this.fContainerControl.onNavigate = SearchResultsScreen.onNavigate;
 
 	var oControl;
+
+	if(showSearchList.length > 0)
+	{
+		this.newControl(new ButtonControl(SearchResultsScreen.SortByNameID, this.ScreenID));
+		this.newControl(new ButtonControl(SearchResultsScreen.SortByPriceID, this.ScreenID));
+	}
 
 	oControl = new ShowSearchListControl(SearchResultsScreen.ShowListID, this.ScreenID,
 		6, oRowItemList, showSearchList);
 	if(showSearchList.length > 0)
 		this.newControl(oControl);
 	oControl.show(showSearchList.length > 0);
+
+	if(showSearchList.length > 0)
+	{
+		this.newControl(new TextControl(SearchResultsScreen.NameID, this.ScreenID));
+		this.newControl(new TextControl(SearchResultsScreen.EpisodeNameID, this.ScreenID));
+		this.newControl(new TextControl(SearchResultsScreen.ProviderID, this.ScreenID));
+	}
 
 	oControl = new TextControl(SearchResultsScreen.NoShowsTextID, this.ScreenID);
 	if(showSearchList.length == 0)
@@ -61,7 +82,6 @@ function SearchResultsScreen(/*Array*/ showSearchList)
 		var oShowSearchListControl = this.getControl(SearchResultsScreen.ShowListID);
 		var oShowSearch = oShowSearchListControl.getFocusedItemValue();
 		var showProviderList = oShowSearch.ShowProviderList;
-		var providerID;
 
 		if(showProviderList.length == 1)
 		{
@@ -79,7 +99,84 @@ function SearchResultsScreen(/*Array*/ showSearchList)
 		return;
 	}
 
+	if(controlID == SearchResultsScreen.SortByNameID)
+	{
+		showMsg("Not yet implemented");	//TODO: need to implement
+		return;
+	}
+
+	if(controlID == SearchResultsScreen.SortByPriceID)
+	{
+		showMsg("Not yet implemented");	//TODO: need to implement
+		return;
+	}
+
 	Screen.prototype.onButton.call(this, controlID);
+}
+
+/******************************************************************************/
+
+/*void*/ SearchResultsScreen.prototype.onListItem = function(/*string*/ controlID)
+{
+	var oSession = MainApp.getThe().getSession();
+
+	if(controlID == SearchResultsScreen.ShowListID)
+	{
+		var oShowSearchListControl = this.getControl(SearchResultsScreen.ShowListID);
+		var oShowSearch = oShowSearchListControl.getFocusedItemValue();
+		var showProviderList = oShowSearch.ShowProviderList;
+		var tempStr = "";;
+
+		this.getControl(SearchResultsScreen.NameID).setText(oShowSearch.Name);
+		this.getControl(SearchResultsScreen.EpisodeNameID).setText(oShowSearch.EpisodeName);
+
+		for(var i = 0; i < showProviderList.length; i++)
+		{
+			if(testStrHasLen(tempStr))
+				tempStr += ", ";
+			tempStr += oSession.getProviderName(showProviderList[i].ProviderID);
+			tempStr += " (" + showProviderList[i].ShowCost.CostDisplay + ")";
+		}
+		this.getControl(SearchResultsScreen.ProviderID).setText(tempStr);
+		return;
+	}
+
+	Screen.prototype.onButton.call(this, controlID);
+}
+
+/******************************************************************************/
+
+/*string*/ SearchResultsScreen.onNavigate = function(/*string*/ fromControl, /*int*/ key)
+{
+	if(fromControl == SearchResultsScreen.ShowListID)
+	{
+		if(key == ek_LeftButton)
+			return SearchResultsScreen.SortByNameID;
+		if(key == ek_DownButton)
+			return ViewPortControl.ControlID;
+	}
+
+	if(fromControl == SearchResultsScreen.SortByNameID)
+		if(key == ek_RightButton)
+			return SearchResultsScreen.ShowListID;
+
+	if(fromControl == SearchResultsScreen.SortByPriceID)
+	{
+		if(key == ek_RightButton)
+			return SearchResultsScreen.ShowListID;
+		if(key == ek_DownButton)
+			return ViewPortControl.ControlID;
+	}
+
+	if(fromControl == ViewPortControl.ControlID)
+	{
+		if(key == ek_RightButton)
+			return SearchResultsScreen.ShowListID;
+		if(key == ek_UpButton)
+			return SearchResultsScreen.SortByPriceID;
+	}
+
+	return null;
 }
 
 /******************************************************************************/
