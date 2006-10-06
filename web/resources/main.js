b2168e4cdabb24bafb954401ce90dcc754ab1ee3
grtvd <grtvd@users.noreply.github.com>
2006-10-06 14:32:21 -0400

    mce: Cut version 2006-10-06.

--- web/resources/main.js
+++ web/resources/main.js
@@ -5957,6 +5957,9 @@ function RentedShow(reader)
 
 License.ShowURLMaxLength = 4096;
 License.LicenseURLMaxLength = 4096;
+License.ContentIDMaxLength = 64;
+License.UserIDMaxLength = 64;
+License.PasswordMaxLength = 32;
 
 /******************************************************************************/
 
@@ -5965,6 +5968,9 @@ function License(reader)
 	this.LicenseMethod = null;
 	this.ShowURL = null;
 	this.LicenseURL = null;
+	this.ContentID = null;
+	this.UserID = null;
+	this.Password = null;
 
 	if(reader != undefined)
 		this.readFrom(reader);
@@ -5977,6 +5983,9 @@ function License(reader)
 	this.LicenseMethod = reader.readString("LicenseMethod", LicenseMethodMaxLength);
 	this.ShowURL = reader.readString("ShowURL", License.ShowURLMaxLength);
 	this.LicenseURL = reader.readString("LicenseURL", License.LicenseURLMaxLength);
+	this.ContentID = reader.readString("ContentID", License.ContentIDMaxLength);
+	this.UserID = reader.readString("UserID", License.UserIDMaxLength);
+	this.Password = reader.readString("Password", License.MaxLength);
 }
 
 /******************************************************************************/
